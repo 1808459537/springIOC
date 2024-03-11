c202dc19321a6d35dfffd4124046b3f0755852a7
ZHT <71699103+1808459537@users.noreply.github.com>
2024-03-11 21:47:21 +0800

    1

--- src/test/java/com/zht/springframework/UserDao.java
+++ src/test/java/com/zht/springframework/UserDao.java
@@ -10,6 +10,7 @@ public class UserDao {
         hashMap.put("10001", "张三");
         hashMap.put("10002", "李四");
         hashMap.put("10003", "王五");
+
     }
 
     public String queryUserName(String uId) {
