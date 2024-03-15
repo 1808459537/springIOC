77bc7865fd59e80fdcbba34feecedbb5da465f90
ZHT <71699103+1808459537@users.noreply.github.com>
2024-03-15 14:48:17 +0800

    FactoryBean

--- src/test/java/com/zht/springframework/ProxyBeanFactory.java
+++ src/test/java/com/zht/springframework/ProxyBeanFactory.java
@@ -12,9 +12,9 @@ public class ProxyBeanFactory implements FactoryBean<IUserDao> {
     public IUserDao getObject() {
         InvocationHandler handler = ((proxy, method, args) -> {
             Map<String ,String> hashMap = new HashMap<>();
-            hashMap.put("10001", "小傅哥");
-            hashMap.put("10002", "八杯水");
-            hashMap.put("10003", "阿毛");
+            hashMap.put("10001", "张三");
+            hashMap.put("10002", "李四");
+            hashMap.put("10003", "王五");
             return "你被代理了 " + method.getName() + "：" + hashMap.get(args[0].toString());
         });
         return (IUserDao) Proxy.newProxyInstance(Thread.currentThread().getContextClassLoader(), new Class[]{IUserDao.class}, handler);
